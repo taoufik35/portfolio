3a86a7c6dcaba7b9239919ceb44b0bfe2d8a09de
unknown <aitdra.taoufik@gmail.com>
2021-05-05 17:05:48 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Nav from './component/layout/nav/Nav';
+import Header from './component/layout/header/Header';
+import Anime from 'react-anime';
+
+
+
+
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Nav />
- 
+      <Header />
+    
     </div>
   );
 }
--- src/component/layout/nav/Nav.js
+++ src/component/layout/nav/Nav.js
@@ -3,25 +3,32 @@ import logoNav from './logoNav.png'
 
 function Nav(){
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <img className="himage" src={logoNav} alt="picture nav " />
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link textColor-white" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">A propos de moi</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-          </ul>
+        
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className = "container">
+            <img className="himage" src={logoNav} alt="picture nav " />
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                    <a className="nav-link textColor-white" href="#">Accueil</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Me découvrir</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Projets</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
+      
     )
 }
 
