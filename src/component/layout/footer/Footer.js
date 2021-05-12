d1f08ae288586bda80e334034db8a8fae7c595d6
unknown <aitdra.taoufik@gmail.com>
2021-05-12 11:24:40 +0200

    correction

--- src/component/layout/footer/Footer.js
+++ src/component/layout/footer/Footer.js
@@ -10,7 +10,7 @@ function Footer(){
             <div className ="icone">
                 <div className ="social">
                     <a 
-                    href ="https://www.facebook.com/taoufik.aitdra/">
+                    href ="https://www.facebook.com/taoufik.aitdra/" target="_blank">
                     <FaFacebook size = "2em" color= "darkblue" />
                     </a>
 
@@ -18,7 +18,7 @@ function Footer(){
 
                 <div className ="social">
 
-                <a href="https://github.com/taoufik35">
+                <a href="https://github.com/taoufik35"target="_blank" >
                 < FaGithub  size = "2em" color = "black" /> 
                 </a>
 
@@ -26,14 +26,14 @@ function Footer(){
 
                 <div className ="social">
 
-                <a href= "https://twitter.com/TaoufikDra">
+                <a href= "https://twitter.com/TaoufikDra" target="_blank">
                 < FaTwitter  size = "2em" />
                 </a>
 
                 </div>
 
                 <div className ="social">
-                <a href= "https://www.linkedin.com/in/ait-dra-taoufik-b23173207/">
+                <a href= "https://www.linkedin.com/in/ait-dra-taoufik-b23173207/" target="_blank">
                 < FaLinkedin  size = "2em"/>
                 </a>
 
